57ef45a53393dd356cf8b4aaea7da19506dd9741
pooya.oa <pooya.oladazimi@tib.eu>
2021-06-01 10:47:49 +0200

    first commit

---
+++ ckanext/upgrade_dataset/plugin.py
@@ -0,0 +1,26 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
+
+
+class MediaWikiLinkPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic','upgrade_dataset')
+        toolkit.add_resource('public/statics', 'ckanext-upgrade-dataset')
+
+
+    #plugin Blueprint
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)
+        blueprint.template_folder = u'templates'
+
+        return blueprint
