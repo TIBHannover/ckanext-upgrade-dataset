156ad0bfe0ad9139b0cdfecbd74bb8338fe25923
pooya.oa <pooya.oladazimi@tib.eu>
2021-06-07 10:19:50 +0200

    removed the checkbox for adding manual machine link

--- ckanext/upgrade_dataset/public/statics/machines.js
+++ ckanext/upgrade_dataset/public/statics/machines.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
     $('.machine_dropdown').parent().parent().find('label').hide(); 
     
-    $('.notFindCheckBox').change(function(){
-        let id = $(this).attr('id');
-        id = id[id.length - 1];
-        if($(this).is(':checked')){
-            $('#manual_link_box' + id).fadeIn();
-            $('#machines_dropdown' + id).fadeOut();
-        }
-        else{
-            $('#manual_link_box' + id).fadeOut();
-            $('#machines_dropdown' + id).fadeIn();
-        }
-    });
+    // $('.notFindCheckBox').change(function(){
+    //     let id = $(this).attr('id');
+    //     id = id[id.length - 1];
+    //     if($(this).is(':checked')){
+    //         $('#manual_link_box' + id).fadeIn();
+    //         $('#machines_dropdown' + id).fadeOut();
+    //     }
+    //     else{
+    //         $('#manual_link_box' + id).fadeOut();
+    //         $('#machines_dropdown' + id).fadeIn();
+    //     }
+    // });
 
 
 
