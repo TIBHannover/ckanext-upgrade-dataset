e72d6ead0a1e8aea8da2ff10862cb56f27e5dd48
pooya.oa <pooya.oladazimi@tib.eu>
2021-06-28 11:46:31 +0200

    media_wiki: prototype for the equipment image preview in a box below the drop-down list

--- ckanext/upgrade_dataset/public/statics/machines.js
+++ ckanext/upgrade_dataset/public/statics/machines.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $('.machine_dropdown').parent().parent().find('label').hide();     
-    $('.machine_dropdown').select2();
+    $('.machine_dropdown').select2();    
     $('.machine_dropdown').change(function(){
         let id = $(this).attr('id');
         id = id[id.length - 1];
         $('#machine_name_' + id).val($(this).find(":selected").text());
         if($(this).find(":selected").val() != '0'){
             let id = '#' + $(this).find(":selected").text().replace(/ /g, "_").replace(/\//g, "_");
-            let image_url = $(id).text();
-            console.info(id);                                 
-            $('#machine-preview').attr('src', image_url);
-            $('#machine_image_modal').modal('show');
+            let image_url = $(id).text(); 
+            let resource_id = $(this).attr('id').split('machines_dropdown_')[1];
+            $('#image_div_' + resource_id).find('img').attr('src', image_url);
+            $('#image_div_' + resource_id).fadeIn();            
         }
         
     });
