0effad3a8ad474bfd9c02bd31a5261908d01901b
pooya.oa <pooya.oladazimi@tib.eu>
2021-07-13 10:38:12 +0200

    link_publication: created the controller and blueprint

---
+++ ckanext/upgrade_dataset/controllers/link_publication.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+
+
+class LinkPublicationController():
+
+    def save_doi():
+        pass
--- ckanext/upgrade_dataset/plugin3.py
+++ ckanext/upgrade_dataset/plugin3.py
@@ -1,16 +1,29 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from flask import Blueprint
-
+from ckanext.upgrade_dataset.controllers.link_publication import LinkPublicationController
 
 
 class LinkPublicationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IBlueprint)
-
-    # IConfigurer
+    
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'themes/link_publication')
         toolkit.add_public_directory(config_, 'statics/link_publication')       
         toolkit.add_resource('statics/link_publication', 'ckanext-upgrade-dataset-link-publication')
+    
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)        
+       
+        blueprint.add_url_rule(
+            u'/upgrade_dataset/save_doi',
+            u'save_doi',
+            LinkPublicationController.save_doi,
+            methods=['POST']
+            )
+
+        return blueprint
