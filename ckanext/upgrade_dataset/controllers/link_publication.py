d5625acd526c026bf7a7e3a45ed5ac9079c63136
pooya.oa <pooya.oladazimi@tib.eu>
2021-08-30 12:10:13 +0200

    link publication: fixed the Bug with reference delete button for non-authorized users

--- ckanext/upgrade_dataset/controllers/link_publication.py
+++ ckanext/upgrade_dataset/controllers/link_publication.py
@@ -15,7 +15,8 @@ class LinkPublicationController():
     def save_doi():
         package_id = request.form.get('package_id')       
         doi = request.form.get('doi')       
-        Helper.check_access_edit_package(package_id)
+        if not Helper.check_access_edit_package(package_id):
+            toolkit.abort(403, 'You are not authorized to access this function')
 
         if package_id and doi and Helper.check_doi_validity(doi) == True:
             package = toolkit.get_action('package_show')({}, {'name_or_id': package_id})
@@ -33,6 +34,7 @@ class LinkPublicationController():
 
     def get_publication(name):        
         res_object = PackagePublicationLink(package_name=name)
+        package = toolkit.get_action('package_show')({}, {'name_or_id': name})
         result = res_object.get_by_package(name=name)
         return_rows = ""
         if result == false:
@@ -41,7 +43,7 @@ class LinkPublicationController():
             meta_data = Helper.process_doi_link(source.doi)
             if meta_data:
                 meta_data['link'] = source.doi
-                return_rows += Helper.create_table_row(meta_data, source.id)
+                return_rows += Helper.create_table_row(meta_data, source.id, Helper.check_access_edit_package(package['id']))
         
         if return_rows != "":
             return return_rows
@@ -66,7 +68,8 @@ class LinkPublicationController():
         doi_obj = res_object.get_by_id(id=doi_id)
         package_name = doi_obj.package_name
         package = toolkit.get_action('package_show')({}, {'name_or_id': package_name})
-        Helper.check_access_edit_package(package['id'])
+        if not Helper.check_access_edit_package(package['id']):
+            toolkit.abort(403, 'You are not authorized to access this function')
         try:            
             doi_obj.delete()
             doi_obj.commit()
